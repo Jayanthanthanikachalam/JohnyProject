a5b18d832cca6d5129bcb010286c158db9ae62da
Johny <Johny@LAPTOP-SLCCJVSV>
2022-05-01 00:05:03 +0530

    ComitMessage

---
+++ JohnyProject/src/com/org/Johny/JohnyProject.java
@@ -0,0 +1,14 @@
+package com.org.Johny;
+
+public class JohnyProject {
+	
+	public static void main(String[] args) {
+		String str = "Jayanthan";
+		for(int i=str.length();i>0;i--){
+			char c = str.charAt(i-1);
+			System.out.print(c);
+			
+		}
+	}
+
+}
